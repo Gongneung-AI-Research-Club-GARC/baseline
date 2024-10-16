e0c5cd07158491992f04df0bfd0a1b401f75a443
AKJ457 <167397778+AKJ457@users.noreply.github.com>
2024-10-16 16:33:45 +0900

    Update 전처리.py

--- 전처리.py
+++ 전처리.py
@@ -2,17 +2,17 @@ import pandas as pd
 
 def preprocess_imu_data(input_file, output_file):
     # Load the dataset with the first row as header
-    imu_data = pd.read_csv(input_file, header=1)
+    imu_data = pd.read_csv(input_file, header=0)
 
     # Select the columns related to Ax, Ay, Az, Gx, Gy, Gz and the label column
-    selected_columns = ['Ax', 'Ay', 'Az', 'Gx', 'Gy', 'Gz', 'Unnamed: 69']  # Last column is the label
+    selected_columns = ['Ax', 'Ay', 'Az', 'Gx', 'Gy', 'Gz', 'Activity_Label']  # Last column is the label
 
     # Filter the dataset with selected columns
     imu_data_filtered = imu_data[selected_columns]
 
     # Define label mapping (e.g., 'walking' -> 0, 'running' -> 1, 'Sitting' -> 2, 'Standing' -> 3, 'Downstairs' -> 4, 'Upstairs' -> 5)
     label_mapping = {'walking': 0, 'running': 1, 'Sitting': 2, 'Standing': 3, 'Downstairs': 4, 'Upstairs': 5 }  # Add more labels as needed
-    imu_data_filtered['Unnamed: 69'] = imu_data_filtered['Unnamed: 69'].map(label_mapping)
+    imu_data_filtered['Activity_Label'] = imu_data_filtered['Activity_Label'].map(label_mapping)
 
     # Drop any rows with missing data
     imu_data_filtered = imu_data_filtered.dropna()
